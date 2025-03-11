4828cd17da331c572fe88df9bbd6071f0e32081d
Oceane <oceane.dqn@gmail.com>
2025-03-11 21:46:20 +0100

    Initial commit

---
+++ okeah-app/pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+
+    <article v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body.slice(0,1000) }}</p>
+      <p><NuxtLink :to="'/blog/'+ post.id">Read more</NuxtLink></p>
+      </article>
+  </div>
+</template>
+
+<script setup>
+
+const {data: posts} = await useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+</script>
